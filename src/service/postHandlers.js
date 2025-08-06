031f111d7eac7aaad9047d9deefa849dda07d166
Michael Adlung <michael.adlung@ibm.com>
2025-08-06 14:36:37 +0200

    fix: resolve merge conflicts

---
+++ src/service/postHandlers.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright IBM Corp. 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * This file contains the functions that do async network requests
+ */
+
+export const getPost = async ({ params: { id } }, res) => {
+  console.log(id);
+
+  try {
+    const response = await fetch(
+      // TODO: replace with actual endpoint URL
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+    // The sample endpoint returns a blogpost
+    const blogpost = await response.json();
+
+    // Return the blogpost's title
+    res.json({ blogpost });
+  } catch {
+    res.status(500).json({ message: 'Failed to fetch message' });
+  }
+};
+
+export const getComments = async ({ query: { postId } }, res) => {
+  try {
+    const response = await fetch(
+      // TODO: replace with actual endpoint URL
+      `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
+    );
+    // The sample endpoint returns a blogpost
+    const comments = await response.json();
+
+    // Return the blogpost's title
+    res.json({ comments });
+  } catch {
+    res.status(500).json({ message: 'Failed to fetch message' });
+  }
+};
+
+export default {
+  getComments,
+  getPost,
+};
