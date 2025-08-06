60dfcd192c0d279b6292e8c253e797e12654ef47
Michael Adlung <michael.adlung@ibm.com>
2025-08-06 16:41:16 +0200

    Refactored network logic to move it out of the hook

--- src/pages/welcome/post/PostComponent.jsx
+++ src/pages/welcome/post/PostComponent.jsx
@@ -1,49 +1,62 @@
 import { getComments, getPost } from '../../../api/message.js';
-import { Heading, Section, Tile } from '@carbon/react';
+import { Heading, Layer, Section, Tile } from '@carbon/react';
 import { useEffect, useState } from 'react';
 
 const PostComponent = () => {
   const [post, setPost] = useState();
   const [comments, setComments] = useState([]);
 
+  const loadPost = async () => {
+    try {
+      const post = await getPost(1);
+      setPost(post);
+    } catch {
+      setPost('Failed to load message');
+    }
+  };
+
+  const loadComments = async () => {
+    try {
+      const comments = await getComments(1);
+      setComments(comments);
+    } catch {
+      setPost('Failed to load comments');
+    }
+  };
+
   useEffect(() => {
-    const loadPost = async () => {
-      try {
-        const post = await getPost(1);
-        setPost(post);
-      } catch {
-        setPost('Failed to load message');
-      }
-    };
-
-    const loadComments = async () => {
-      try {
-        const comments = await getComments(1);
-        setComments(comments);
-      } catch {
-        setPost('Failed to load comments');
-      }
-    };
-
     loadPost();
     loadComments();
   }, []);
 
   return (
-    <Tile>
-      <Section as="article" level={3}>
-        <Heading>{post?.title ?? 'Loading...'}</Heading>
-      </Section>
-      <Section as="article" level={4}>
-        <Heading>Comments</Heading>
-        {comments?.map((comment) => (
-          <Tile title={`Post from ${comment.email}`} key={comment.id}>
-            <Heading>{`From ${comment.email}`}</Heading>
-            <div>{comment.body}</div>
-          </Tile>
-        ))}
-      </Section>
-    </Tile>
+    <Section as="article" level={3}>
+      <Heading>Posts</Heading>
+      <Tile>
+        <Section as="article" level={3}>
+          <Section level={4}>
+            <Heading>{post?.title ?? 'Loading...'}</Heading>
+            <div>{post?.body}</div>
+          </Section>
+        </Section>
+
+        <Section as="article" level={3}>
+          <Heading>Comments</Heading>
+          <Section as="article" level={4}>
+            <Layer level={1}>
+              <Tile>
+                {comments?.map((comment) => (
+                  <Tile title={`Post from ${comment.email}`} key={comment.id}>
+                    <Heading>{`From ${comment.email}`}</Heading>
+                    <div>{comment.body}</div>
+                  </Tile>
+                ))}
+              </Tile>
+            </Layer>
+          </Section>
+        </Section>
+      </Tile>
+    </Section>
   );
 };
 
