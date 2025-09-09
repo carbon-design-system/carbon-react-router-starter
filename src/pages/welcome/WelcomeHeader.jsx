51e7d497558db44ed9c1374729739a92d8e0029b
Lee Chase <lee.chase@uk.ibm.com>
2025-09-09 09:59:49 +0100

    Merge branch 'main' into routesAsConfig2

--- src/pages/welcome/WelcomeHeader.jsx
+++ src/pages/welcome/WelcomeHeader.jsx
@@ -22,7 +22,12 @@ export const WelcomeHeader = () => {
         </>,
         <>Maintained by fed-at-ibm, a chapter of the OIC.</>,
         <>
-          <Button renderIcon={ArrowRight}>Clone the template</Button>
+          <Button
+            renderIcon={ArrowRight}
+            href="https://github.com/carbon-design-system/carbon-react-router-starter/generate"
+          >
+            Use this template
+          </Button>
         </>,
       ]}
     />
