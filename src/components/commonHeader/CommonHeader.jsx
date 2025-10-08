bb8bea577c42308545dcea41b6b7a8867f3f474d
Jan Hassel <Jan.Hassel@ibm.com>
2025-10-08 16:22:13 +0200

    fix: apply basic semantic html

--- src/components/commonHeader/CommonHeader.jsx
+++ src/components/commonHeader/CommonHeader.jsx
@@ -9,9 +9,9 @@ import { AspectRatio, Column, Grid } from '@carbon/react';
 
 export const CommonHeader = ({ title, paragraphs }) => {
   return (
-    <Grid className="cs--common-header">
+    <Grid as="header" className="cs--common-header">
       <Column sm={4} md={8} lg={8}>
-        <AspectRatio as="section" ratio="16x9">
+        <AspectRatio ratio="16x9">
           <h1 className="cs--common-header__title">{title}</h1>
           {paragraphs.map((paragraph, i) => (
             <p key={`common-header-paragraph-${i}`}>{paragraph}</p>
