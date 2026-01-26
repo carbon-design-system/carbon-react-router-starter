aa36041b51368acbe9bf9b921ab05ca820d27b64
Lee Chase <lee.chase@uk.ibm.com>
2026-01-26 18:14:04 +0000

    fix: unused var

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type { Express, RequestHandler } from 'express';
+import type { RequestHandler } from 'express';
 import { getPost, getComments } from '../service/postHandlers';
 
 /**
