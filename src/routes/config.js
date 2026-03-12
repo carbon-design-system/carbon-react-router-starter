a446a4335c18b1f2826fd800a96d89510d62d692
Stephane Rodet <RODET@de.ibm.com>
2026-03-12 08:40:11 +0100

    chore: add comment on code splitting
    Also update the copyright

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corp. 2025
+ * Copyright IBM Corp. 2025, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,6 +7,10 @@
 import { lazy } from 'react';
 import { MagicWand, LogoGithub } from '@carbon/icons-react';
 
+// This project splits the pages each in different JavaScript bundles.
+// You might also split part of the pages that incur significant
+// network load at your discretion, given they are not needed right
+// away when the page loads.
 const Dashboard = lazy(() => import('../pages/dashboard/Dashboard'));
 const NotFound = lazy(() => import('../pages/not-found/NotFound'));
 const Placeholder = lazy(() => import('../pages/placeholder/Placeholder'));
